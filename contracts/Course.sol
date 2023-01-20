b81d8f340c4058bf05f31750c4dd74f5ee72b8c3
DinizSa <39380608+DinizSa@users.noreply.github.com>
2023-01-20 23:11:14 +0000

    Add open and closed state with modifiers

--- contracts/Course.sol
+++ contracts/Course.sol
@@ -3,8 +3,23 @@ pragma solidity ^0.8.17;
 
 contract Course {
     string public name;
+    enum Status {Open, Closed}
+    Status public status = Status.Open;
 
     constructor(string memory _courseName) {
         name = _courseName;
+    }
+
+    function closeCourse() public onlyOpenCourse {
+        status = Status.Closed;
+    }
+
+    modifier onlyOpenCourse() {
+        require(status == Status.Open, "Course not open");
+        _;
+    }
+    modifier onlyClosedCourse() {
+        require(status == Status.Closed, "Course not closed");
+        _;
     }
 }
